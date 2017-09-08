55d67a50239016de17f9262ea8c7be93ba415fb6
georgecode <georgeczurlanis@gmail.com>
2017-09-08 00:32:55 -0700

    Add submitTodo reducer and unit test for submitTodo reducer

---
+++ src/reducer/index.js
@@ -0,0 +1,35 @@
+// export const initialState = {};
+
+// export const reducer = (state = initialState, action) => state;
+
+// export default reducer;
+
+
+
+import types from '../constants/';
+
+export const initialState = {
+  todos: [],
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case types.SUBMIT_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: action.id,
+            text: action.text,
+          },
+        ],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
